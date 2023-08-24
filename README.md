b26975ff1c838c685ea35aa72056fd219b41faf6
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-23 20:07:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PortalColaborador
+ 
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+        Background: "",        
+        
+        Blue: "#103778",
+        Blue_navy: "#151F30",
+
+    }
+}
