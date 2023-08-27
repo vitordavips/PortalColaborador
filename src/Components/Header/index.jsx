8ef880484ebce858cfc84f02ca736fd2c521f9b4
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-27 17:26:52 -0400

    configurando o header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 export function Header(){
     return(
         <Container>
-            
+            <img src="../../Imagens/Logo_Digiboard.jpg" alt="" />
         </Container>
-    )
+    );
 }
--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,11 +1,14 @@
 import { Container } from './styles';
 
+import { Header } from '../../Components/Header';
 import { Button } from '../../Components/Button';
 
 
 export function Details(){
     return(
         <Container>
+            <Header />
+
             <h1>Dados Pessoais</h1>
             <Button title="Meus Riscos Ocupacionais" />
             <Button title="Meus Exames Periódicos" />
